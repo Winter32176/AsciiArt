5b699d8b761633dac57d7b728e0dc020d11b3b3c
JavaCourses1 <robert321766@outlook.com>
2020-02-06 20:56:52 +0200

    V4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,24 +19,25 @@ public class Main {
         for (int n = 0; n < row; n++) {
 
 
+            for (int p = 0; p <colums; p++) {
 
 
-                for (int m = 0; m < height; m++) {
+            for (int m = 0; m < height * p; m++) {
 
-                    for (int z = 0; z < width; z++) {
+                for (int z = 0; z < width * p; z++) {
 
-                        if (z == 0 || z == width-1 || m == 0 || m == height-1) {
-                            System.out.print("*");
-                        } else {
-                            System.out.print(" ");
-                        }
-
+                    if (z == width * p || z == width - 1 * p || m == height*p || m == height - 1 * p {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
                     }
 
-                    System.out.println();
                 }
 
+                System.out.println();
+            }
 
+        }
 
 
 
