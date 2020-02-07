edbdd7a96e3435d8c48c090192972358d5d54c5a
rober <robert321766@outlook.com>
2020-02-07 15:27:59 +0200

    V7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,31 +19,27 @@ public class Main {
         for (int n = 0; n < row; n++) {
 
 
-            for (int p = 0; p <colums; p++) {
+            for (int m = 0; m < height; m++) {
+                for (int p = 0; p < colums; p++) {
+                    for (int z = 0; z < width; z++) {
 
 
-            for (int m = 0; m < height * p; m++) {
 
-                for (int z = 0; z < width * p; z++) {
+                        if (z == 0||z== width - 1|| m == 0 || m == height-1) {
+                            System.out.print("*");
+                        } else {
+                            System.out.print(" ");
+                        }
 
-                    if (z == width * p || z == width - (1 * p) || m == height*p || m == height - (1 * p)) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
                     }
-
+                   System.out.print(" ");
                 }
 
                 System.out.println();
             }
 
-        }
 
-
-
-
-
-            System.out.println("-");
+            System.out.println();
         }
 
     }
